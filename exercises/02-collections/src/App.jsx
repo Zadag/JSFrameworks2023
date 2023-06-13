56e7b1c8c703fe3ad25d8b490d8d08e2485ea478
Kevin Browne <kbrowne97@gmail.com>
2023-06-12 20:00:34 -0400

    002 changes

--- exercises/02-collections/src/App.jsx
+++ exercises/02-collections/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const technologiesJSX = technologies.map((img) => <img src={img}></img>);
   const phonesJSX = Object.values(phones).map((img) => <img src={img}></img>);
 
-  return <div className="App">{[technologiesJSX, phonesJSX]}</div>;
+  return <div className="App">{[...technologiesJSX, ...phonesJSX]}</div>;
 }
 
 export default App;
