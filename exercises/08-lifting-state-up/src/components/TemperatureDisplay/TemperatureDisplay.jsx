3308dc67e9ac2a75ceb979c6994b1ec827693d17
Kevin Browne <kbrowne97@gmail.com>
2023-07-03 19:19:21 -0400

    adds solution to Temperature exercise

--- exercises/08-lifting-state-up/src/components/TemperatureDisplay/TemperatureDisplay.jsx
+++ exercises/08-lifting-state-up/src/components/TemperatureDisplay/TemperatureDisplay.jsx
@@ -1,8 +1,15 @@
 import PropTypes from "prop-types";
 
-function TemperatureDisplay(props) {
-  return <>{/* Complete me */}</>;
-}
+const TemperatureDisplay = ({ temperature, scale }) => {
+  return (
+    <>
+      <div className="h4">Current Temperature:</div>
+      <div className="h1">
+        {temperature} &deg; {scale}
+      </div>
+    </>
+  );
+};
 
 TemperatureDisplay.propTypes = {
   temperature: PropTypes.number.isRequired,
--- exercises/08-lifting-state-up/src/components/TemperatureScale/TemperatureScale.jsx
+++ exercises/08-lifting-state-up/src/components/TemperatureScale/TemperatureScale.jsx
@@ -1,9 +1,17 @@
 import PropTypes from "prop-types";
 
-function TemperatureScale(props) {
-  // Complete me
-  return <></>;
-}
+const TemperatureScale = ({ setScale }) => {
+  return (
+    <div className="btn-group" role="group" aria-label="Convert temperature">
+      <button className="btn btn-outline-primary" onClick={() => setScale("F")}>
+        Fahrenheit
+      </button>
+      <button className="btn btn-outline-primary" onClick={() => setScale("C")}>
+        Celsius
+      </button>
+    </div>
+  );
+};
 
 TemperatureScale.propTypes = {
   setScale: PropTypes.func.isRequired,
